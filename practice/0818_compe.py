ec25c784d435734b4a2ab71bad2ebf5dd62f694d
YutoNakano <yuto.44.bb@gmail.com>
2019-08-18 21:56:10 +0900

    add: A問題を2もんやった

---
+++ practice/0818_compe.py
@@ -0,0 +1,15 @@
+a = int(input())
+s = input()
+
+if a >= 3200:
+    print(s)
+else:
+    print('red')
+
+# A - Harmony
+c,d = map(int,input().split())
+
+if (c+d)%2 == 0:
+    print((c+d)//2)
+else:
+    print('IMPOSSIBLE')
